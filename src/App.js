e4ab083b6a80a883cf0ec45d7199247285220e40
Alex N <xander.nesta@gmail.com>
2023-06-26 19:30:15 -0400

    add html footer and fontawesome icon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import Board from './components/Board.js';
 import Header from './components/Header.js'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
+
+
 
 function App() {
   return (
     <div className="App" id="App">
       <Header></Header>
       <Board></Board>
+      <footer class="footer">
+        <p>Xander© 2022</p>
+        <a href="https://github.com/xandernesta/odin-memory-card">
+          <FontAwesomeIcon icon={icon({ name: 'GitHub',style: 'brands'})} />
+        </a>
+      </footer>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,6 @@
+<footer class="footer">
+        <p>Xander© 2022</p>
+        <a href="https://github.com/xandernesta/odin-battleship">
+            <i class="fa-github fa-brands" alt="github icon"></i>
+        </a>
+    </footer>
